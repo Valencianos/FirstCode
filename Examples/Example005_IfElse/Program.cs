82fde704fdf19703a76db9fa3e02384cea4e4dd6
Valencianos <malina_mami@mail.ru>
2023-01-25 15:13:47 +0300

    If or Else

---
+++ Examples/Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter Username: ");
+string Username = Console.ReadLine();
+
+if(Username.ToLower == "maria")
+{
+    Console.WriteLine("OMG, It`s MARIA!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(Username);
+}
