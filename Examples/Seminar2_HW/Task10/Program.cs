6307a272d0601236b0dc965f0666a7251a5acbcd
Valencianos <malina_mami@mail.ru>
2023-01-30 16:20:48 +0300

    Homework for Seminar 2

---
+++ Examples/Seminar2_HW/Task10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Enter a three-digit number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if ((a < 99) || (a > 1000))
+{
+    Console.WriteLine("Incorrect number. Please, enter again");
+}
+else
+{
+    int b = a / 10;
+    int c = b % 10;
+    Console.WriteLine(c);
+}
---
+++ Examples/Seminar2_HW/Task13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Write("Enter number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 100)
+{
+    Console.WriteLine("There is no 3rd digit");
+}
+else
+{
+    while (a >= 1000)
+    {
+        a /= 10;
+    }
+    int result = a % 10;
+    Console.WriteLine(result);
+}
