f7db37f568638737053bc6163db341bbb42b249d
Valencianos <malina_mami@mail.ru>
2023-02-02 12:39:13 +0300

    Homework for 3rd Seminar

---
+++ Examples/Seminar3_HW/Task19/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Enter 5-digits number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+if ((num / 10000 == num % 10) && (((num / 1000) % 10) == (num % 100) / 10)) 
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
---
+++ Examples/Seminar3_HW/Task21/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Write("Enter Ax: ");
+int Ax = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter Ay: ");
+int Ay = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter Az: ");
+int Az = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter Bx: ");
+int Bx = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter By: ");
+int By = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter Bz: ");
+int Bz = Convert.ToInt32(Console.ReadLine());
+
+double length = Math.Round(Math.Sqrt(Math.Pow(Bx - Ax, 2) + Math.Pow(By - Ay, 2) + Math.Pow(Bz - Az, 2)), 2);
+
+Console.WriteLine(length);
