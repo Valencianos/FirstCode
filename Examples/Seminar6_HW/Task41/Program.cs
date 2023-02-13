c5e0423fc6efe3231acaa6854e439b859f7fd30e
Valencianos <malina_mami@mail.ru>
2023-02-13 14:15:04 +0300

    HW for Seminar 6

---
+++ Examples/Seminar6_HW/Task41/Program.cs
@@ -0,0 +1,16 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Enter number: ");
+string a = Convert.ToString(Console.ReadLine());
+
+string[] nums = a.Split(new char[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries);
+int[] n = nums.Select(Int32.Parse).ToArray();
+
+int count = 0;
+for (int i = 0; i < n.Length; i++)
+{
+    if (n[i] > 0) count += 1;
+}
+Console.Write($"->{count} ");
